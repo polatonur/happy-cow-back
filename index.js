01f833bf4b8ec8d78b7759f8b81ecd015c05d6ae
onur <onurpolatplt@gmail.com>
2021-12-19 08:49:01 +0100

    delete unnecessary lines

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const formidableMiddleware = require("express-formidable");
 const cors = require("cors");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 require("dotenv").config();
 // call express
 const app = express();
-// app.use(formidableMiddleware());
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // import routes
 const restaurantRoutes = require("./routes/restaurant");
 app.use(restaurantRoutes);
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
+
 // MONGOOSE CONNECT
 mongoose.connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 // HOME ROUTE
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to my happy cow app" });
+  res.status(200).json({ message: "Welcome to my happy cow app" });
 });
 // ALL ROUTE
 app.all("*", (req, res) => {
-    res.status(404).send("Oops!, Page not found");
+  res.status(404).send("Oops!, Page not found");
 });
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
