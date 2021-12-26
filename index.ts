3f57f56d8746fb154eaaa57b71d3974f9279ec29
onur <onurpolatplt@gmail.com>
2021-12-26 22:59:15 +0100

    fix deploy err

--- index.ts
+++ index.ts
@@ -38,5 +38,5 @@ app.all("*", (req: Request, res: Response) => {
   res.status(404).send("Oops!, Page not found");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
